25e6f6eb5d4b03b2823fd848d173a6d3a8578aac
symbolpunk <103775631+symbolpunk@users.noreply.github.com>
2024-04-24 16:09:06 -0400

    merge conflicts fix

--- website/sidebars.js
+++ website/sidebars.js
@@ -568,7 +568,7 @@ const sidebars = {
           items: [
             {
               type: 'doc',
-              id: 'node-running/how-tos/running-a-feed-relay',
+              id: 'run-arbitrum-node/sequencer/run-feed-relay',
               label: 'Run a feed relay',
             },
             {
